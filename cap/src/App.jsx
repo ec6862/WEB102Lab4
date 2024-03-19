888fc7570baaba5c246e1f083f74d3b15fb9d128
Eric <60475399+ec6862@users.noreply.github.com>
2024-03-19 12:51:59 -0400

    partway done with Step 1

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import APIForm from './components/APIForm';
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
 const App = () => {
+  const [inputs, setInputs] = useState({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
   return (
     <div>
-
+      <APIForm/>
     </div>
   )
 }
---
+++ cap/src/components/APIForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const APIForm = ({inputs, handleChange, onSubmit}) => {
+    return (
+        <div>
+            <h2>Select Your Image Attributes</h2>
+            <form className="form-container">
+                {inputs && Object.entries(inputs).map(([category, value], index) => (
+                    <li className="form" key={index}>
+                        <h2>{category}</h2>
+                        <input
+                            type="text"
+                            name={category}
+                            value={value}
+                            placeholder="Input this attribute..."
+                            onChange={handleChange}
+                            className="textbook"
+                        />
+                        <br></br>
+                        <br></br>
+                        <p>{inputsInfo[index]}</p>
+                    </li>
+                ))}
+            </form>
+        </div>
+    )
+}
+
+export default APIForm
